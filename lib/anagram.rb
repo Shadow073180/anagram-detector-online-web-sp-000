e17749adcbf62cc9a7053a1f1a7fbdbfca816255
David masterson <david73180@gmail.com>
2018-10-14 19:54:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor  :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(array)
+    new = []
+    wordarray = word.split("")
+    gram = Anagram.new(word)
+    # binding.pry
+    array.each do |x|
+      if x.include?(wordarray.each do |y|)
+      end
+        new << x
+      end
+      end
+    end
+     return new
+  end
+
+end
